868f7d682cc68f8daeba3d034d15e4d213975a53
14949 <14949@student.eadania.dk>
2023-02-10 13:51:04 +0100

    test property 2

--- Source/ArenaGame/FirstTestClass.h
+++ Source/ArenaGame/FirstTestClass.h
@@ -12,6 +12,8 @@ class ARENAGAME_API FirstTestClass
 public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Test")
 		int32 testNumber;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Test")
+		int32 testNumber2;
 	FirstTestClass();
 	~FirstTestClass();
 };
