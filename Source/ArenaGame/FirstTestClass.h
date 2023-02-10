42304f1d3a84a7d6d637ab6b14be4ecf87aaba46
PeterrificHC <peterhc01@gmail.com>
2023-02-10 13:17:33 +0100

    Script coding added to project for the first time

---
+++ Source/ArenaGame/FirstTestClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ARENAGAME_API FirstTestClass
+{
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Test")
+		int32 testNumber;
+	FirstTestClass();
+	~FirstTestClass();
+};
